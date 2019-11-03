f620400df476becadc972d9fcfe60f2257969c69
Shubham Jain <shubham.jain@engineerbabu.in>
2019-11-04 02:34:29 +0530

    generating redis key

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,10 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('IM About to run a Query');
+
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+    console.log('Key => ', key);
     return exec.apply(this, arguments);
 }
