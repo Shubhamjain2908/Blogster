0dc5e8108a0e528b568e91c183a478e313549c98
Shubham Jain <shubham.jain@engineerbabu.in>
2019-11-04 02:16:10 +0530

    Patching Mongoose's Exec

---
+++ services/cache.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+    console.log('IM About to run a Query');
+    return exec.apply(this, arguments);
+}
