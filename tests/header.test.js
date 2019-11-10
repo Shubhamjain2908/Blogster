4101fbd80547af968e5233648701a17a8606abd9
Shubham Jain <shubham.jain@engineerbabu.in>
2019-11-11 02:38:50 +0530

    launch a browser using Puppeteer

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,16 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
     const sum = 1 + 2;
 
     expect(sum).toEqual(3);
+});
+
+//For Chromium error => npm install puppeteer --unsafe-perm=true --allow-root
+test('We can launch a browser', async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
 });
