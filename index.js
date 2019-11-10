ed7f0cb5392114b8acf8c9ba90081b63d71deddd
Shubham Jain <shubham.jain@engineerbabu.in>
2019-11-11 01:59:21 +0530

    added morgan for API loging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -14,7 +15,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
-
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(
   cookieSession({
